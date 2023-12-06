2352044db32858d3c37b8729cc16f078db4e3eb5
Akash Nath <114856619+AkashNath1995@users.noreply.github.com>
2023-12-06 19:23:33 +0530

    Update Layout.tsx

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,13 +1,18 @@
-// components/Layout.js
+// Layout.js
+
 import React from 'react';
 import Sidebar from './Sidebar';
 import Form from './Form';
-import styles from '../../styles/Layout.module.css'; 
+import styles from '../../styles/Layout.module.css';
 
 const Layout = () => {
   return (
+    // Overall layout container
     <div className={styles.container}>
+      {/* Sidebar component for navigation */}
       <Sidebar />
+
+      {/* Form component for user interaction */}
       <Form />
     </div>
   );
